af4fc2d90f5e84ad4bd0c3ec729ab1141dd87c0e
Remco Bloemen <remco-git@xn--2-umb.com>
2017-08-23 14:14:49 +0200

    Extract BigNumber

---
+++ contracts/test/GetNumber.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.4.15;
+
+contract GetNumber {
+
+    uint256 public number = 1;
+
+}
--- mocha.js
+++ mocha.js
@@ -1,5 +1,13 @@
-import chai from "chai";
-import chaiAsPromised from "chai-as-promised";
-import chaiBignumber from "chai-bignumber";
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import chaiBignumber from 'chai-bignumber';
 
-chai.use(chaiAsPromised).use(chaiBignumber(web3.BigNumber));
+const GetNumber = artifacts.require('./test/GetNumber.sol');
+
+before(async () => {
+  const getNumber = await GetNumber.new();
+  const number = await getNumber.number.call();
+  const BigNumber = number.constructor;
+  chai.use(chaiAsPromised).use(chaiBignumber(BigNumber));
+  global.BigNumber = BigNumber;
+});
